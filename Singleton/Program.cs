5352cf9c79d5dc56f7a112f6ddfe6610060a4cf5
saman azadi <samanazadi1996@gmail.com>
2021-07-07 15:49:16 +0430

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Singleton Design Patterns");
+
+            var settings = Settings.GetInstance();
+
+            settings.Key = "test";
+            settings.Value = "test";
+
+            Write();
+
+            Console.ReadKey();
+        }
+        static void Write()
+        {
+            var settings = Settings.GetInstance();
+
+            Console.WriteLine($"key : {settings.Key}   -->  Value : {settings.Value} ");
+        }
+    }
+}
---
+++ Singleton/Settings.cs
@@ -0,0 +1,18 @@
+﻿namespace Singleton
+{
+    public class Settings
+    {
+        private Settings()
+        {
+
+        }
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+        private static Settings settings = new Settings();
+        public static Settings GetInstance()
+        {
+            return settings;
+        }
+    }
+}
