44cb526bfc5174a9cff3bac223c359ea5379205b
saman azadi <samanazadi1996@gmail.com>
2021-07-11 23:05:27 +0430

    Add Prototype Design Pattern

---
+++ Prototype/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Prototype
+{
+    public interface ICloneable<T>
+    {
+        T Clone();
+    }
+    public class MyObject : ICloneable<MyObject>
+    {
+        public int Count { get; set; } = 10;
+
+        public MyObject Clone()
+        {
+            return (MyObject)MemberwiseClone();
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyObject MyObject = new MyObject();
+            Console.WriteLine($"MyObject = { MyObject.Count}");
+
+            var instance1 = MyObject.Clone();
+            instance1.Count++;
+            Console.WriteLine($"Instance 1 = { instance1.Count}");
+
+            var instance2 = MyObject.Clone();
+            instance2.Count += 20;
+            Console.WriteLine($"Instance 2 = { instance2.Count}");
+
+            Console.WriteLine($"Instance 1 = { instance1.Count}");
+
+            Console.ReadKey();
+        }
+    }
+}
