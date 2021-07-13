90a45e8afc187a07975d2b3384679ee80c2a95a2
saman azadi <samanazadi1996@gmail.com>
2021-07-13 14:12:53 +0430

    Add Sterategy DesignPattern

---
+++ Sterategy/Calculate.cs
@@ -0,0 +1,30 @@
+﻿namespace Sterategy
+{
+    public abstract class Calculate
+    {
+        public abstract int GetPrice(int price);
+    }
+    public class SilverCalculator : Calculate
+    {
+        public override int GetPrice(int price)
+        {
+            return (int)(price - (price * 0.1));
+        }
+    }
+    public class GoldCalculator : Calculate
+    {
+        public override int GetPrice(int price)
+        {
+            return (int)(price - (price * 0.2));
+        }
+    }
+
+    public class NullCalcutor : Calculate
+    {
+        public override int GetPrice(int price)
+        {
+            return price;
+        }
+    }
+
+}
---
+++ Sterategy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sterategy
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate nullCalcutor = new NullCalcutor();
+            Console.WriteLine($"Ordinary customer final amount = {nullCalcutor.GetPrice(1000)}");
+
+            Calculate goldCalculator = new GoldCalculator();
+            Console.WriteLine($"The final amount of Golden Customer is 20% discount = {goldCalculator.GetPrice(1000)}");
+
+            Calculate silverCalculator = new SilverCalculator();
+            Console.WriteLine($"The final amount of Silver Customer is 10% discount = {silverCalculator.GetPrice(1000)}");
+
+            Console.ReadKey();
+        }
+    }
+}
